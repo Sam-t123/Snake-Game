f598a952370c546263b2c5ca23663127bb1206bd
Samarth Tiwari <tiwarisamarth01@gmail.com>
2021-06-27 13:28:29 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+dis=pygame.display.set_mode((800,600))
+pygame.display.update()
+pygame.display.set_caption('Snake game')
+game_over=False
+
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over=True
+        print(event)
+        
+        
+pygame.quit()
+quit()
+
