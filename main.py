4ed133d22ce83e94a9068fcefd7204354e3e294c
Samarth Tiwari <tiwarisamarth01@gmail.com>
2021-06-27 13:43:38 +0530

    Added food particle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 dis=pygame.display.set_mode((800,600))
@@ -8,13 +9,15 @@ game_over=False
 # color (R,G,B)
 snakeColor = (255,255,255)  # White
 screenColor = (0,0,0)       # Black
+foodColor=(255,0,0)         # Red
 
-
+food_x = 10*random.randint(0,80)
+food_y = 10*random.randint(0,60)
 x = 400
 y = 300
 
-delx=0
-dely=0
+mov_x=0
+mov_y=0
 clock = pygame.time.Clock()
 
 while not game_over:
@@ -24,21 +27,22 @@ while not game_over:
             game_over=True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                delx = 0
-                dely = -10
+                mov_x = 0
+                mov_y = -10
             if event.key == pygame.K_DOWN:
-                delx = 0
-                dely = 10
+                mov_x = 0
+                mov_y = 10
             if event.key == pygame.K_LEFT:
-                delx = -10
-                dely = 0
+                mov_x = -10
+                mov_y = 0
             if event.key == pygame.K_RIGHT:
-                delx = 10
-                dely = 0
-    x += delx
-    y += dely
+                mov_x = 10
+                mov_y = 0
+    x += mov_x
+    y += mov_y
     dis.fill(screenColor)
     pygame.draw.rect(dis,snakeColor,[x,y,10,10])
+    pygame.draw.rect(dis,foodColor,[food_x,food_y,10,10])
     pygame.display.update()
     clock.tick(30)
         
